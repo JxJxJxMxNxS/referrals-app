2341a47142a7e64902030cb62b4eae2693530c56
Jose Juan Juarez Monsivais <jjuarez@nearsoft.com>
2018-07-05 23:02:05 -0500

    Added the token generation

---
+++ src/main/java/com/Nearsoft/referrals/model/User.java
@@ -0,0 +1,4 @@
+package com.Nearsoft.referrals.model;
+
+public class User {
+}
---
+++ src/main/java/com/Nearsoft/referrals/service/impl/AppUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.Nearsoft.referrals.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Component
+public class AppUserDetailsService implements UserDetailsService {
+
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+       return null;
+    }
+}
