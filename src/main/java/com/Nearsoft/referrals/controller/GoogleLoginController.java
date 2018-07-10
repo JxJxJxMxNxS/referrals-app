3d7fad93df770069fdb92614ad6ed273c24a02d1
Jose Juan Juarez Monsivais <jjuarez@nearsoft.com>
2018-07-10 12:32:22 -0500

    merged master with the security_implementation branch

--- src/main/java/com/Nearsoft/referrals/controller/GoogleLoginController.java
+++ src/main/java/com/Nearsoft/referrals/controller/GoogleLoginController.java
@@ -32,6 +32,6 @@ public class GoogleLoginController {
             return tokenGeneratorService.generateToken();
         }
         else
-            return "";
+            return tokenId;
     }
 }
--- src/main/java/com/Nearsoft/referrals/service/impl/RecruiterServiceImpl.java
+++ src/main/java/com/Nearsoft/referrals/service/impl/RecruiterServiceImpl.java
@@ -20,19 +20,7 @@ public class RecruiterServiceImpl implements RecruiterService {
     @Override
     public List<Recruiter> getRecruiters() {
         List<Recruiter> recruitersList = new ArrayList<>();
-       /* Recruiter recruiter1 = new Recruiter();
-        Recruiter recruiter2 = new Recruiter();
 
-        recruiter1.setName("Recruiter1");
-        recruiter1.setEmail("Recruiter1@nearsoft.com");
-        recruiter1.setPicture("");
-        recruiter2.setName("Recruiter2");
-        recruiter2.setEmail("Recruiter2@nearsoft.com");
-        recruiter2.setPicture("");
-
-
-        recruitersList.add(recruiter1);
-        recruitersList.add(recruiter2);*/
         recruiterRepository.findAll().forEach(recruitersList::add);
         return recruitersList;
     }
