8bd3cf66045609dd4b4d8913f349b10ddceccee4
Jose Juan Juarez Monsivais <jjuarez@nearsoft.com>
2018-07-20 09:46:55 -0500

    Added configuration in SecurityConfig to disable the check of the Authorization header in the login endpoint

--- src/main/java/com/nearsoft/referrals/config/SecurityConfig.java
+++ src/main/java/com/nearsoft/referrals/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -77,6 +78,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
         converter.setSigningKey(securitySettings.getSigningKey());
         return converter;
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/login");
     }
 
     @Bean
