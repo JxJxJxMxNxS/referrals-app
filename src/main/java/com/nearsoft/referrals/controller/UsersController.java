559e2a944b9af1064bc62c23470b6b2d67e83e65
Jose Juan Juarez Monsivais <jjuarez@nearsoft.com>
2018-08-13 10:20:32 -0500

    endpoint to return referreds by one user

--- src/main/java/com/nearsoft/referrals/controller/UsersController.java
+++ src/main/java/com/nearsoft/referrals/controller/UsersController.java
@@ -1,24 +1,47 @@
 package com.nearsoft.referrals.controller;
 
+import com.nearsoft.referrals.model.ReferredByUser;
+import com.nearsoft.referrals.model.User;
+import com.nearsoft.referrals.repository.ReferredByUserRepository;
 import com.nearsoft.referrals.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class UsersController {
 
     private UserService userService;
 
-    public UsersController(UserService userService) {
-        this.userService = userService;
-    }
+    private ReferredByUserRepository referredByUserRepository;
 
+    public UsersController(UserService userService, ReferredByUserRepository referredByUserRepository) {
+        this.userService = userService;
+        this.referredByUserRepository = referredByUserRepository;
+    }
 
     @RequestMapping(value = "/user")
     String getUser(Principal principal) {
         return principal.getName();
+    }
+
+    @RequestMapping(value = "/referreds")
+    ResponseEntity<List<ReferredByUser>> referreds(Principal principal) {
+        List<ReferredByUser> ret = referredByUserRepository.findAll();
+        List<ReferredByUser> ret2 = new ArrayList<>();
+        User user = userService.getPrincipalUser(principal.getName());
+        for (ReferredByUser u : ret
+                ) {
+            if (u.getUserId() == user.getId()) {
+                ret2.add(u);
+            }
+        }
+        return new ResponseEntity<>(ret2, HttpStatus.OK);
     }
 
 }
